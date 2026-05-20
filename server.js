a03f69df1e7bd202efe047eb66951a44faad1c40
Flavio <flaviocsantos99@gmail.com>
2026-05-20 19:42:36 -0300

    backend pronto

---
+++ server.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_ID = process.env.WOOVI_API_ID;
+
+app.post("/criar-pix", async (req, res) => {
+
+  console.log("ROTA /criar-pix CHAMADA");
+
+  try {
+
+    const { nome, valor } = req.body;
+
+    const response = await axios.post(
+      "https://api.woovi.com/api/v1/charge",
+      {
+        correlationID: "doacao_" + Date.now(),
+
+        value: Number(valor) * 100,
+
+        comment: "Doacao Site",
+
+        customer: {
+  name: nome || "Doador",
+  email: "doacao@site.com",
+  phone: "11999999999",
+  taxID: "11144477735"
+}
+      },
+      {
+  headers: {
+  "Content-Type": "application/json",
+  Authorization: API_ID
+}
+}
+    );
+
+    console.log(response.data);
+
+    const charge =
+      response.data.charge ||
+      response.data.charges?.[0];
+
+    res.json({
+  paymentId: charge.correlationID,
+  qrCode: charge.brCode,
+  brCode: charge.brCode,
+  qrCodeImage: charge.qrCodeImage,
+  amount: charge.value
+});
+
+  } catch (error) {
+
+    console.log(
+      error.response?.data ||
+      error.message
+    );
+
+    res.status(500).json({
+      erro: "Erro ao gerar PIX"
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend online");
+});
+
+app.get("/consultar-pix", async (req, res) => {
+
+  try {
+
+    const id = req.query.id;
+
+    const response = await axios.get(
+      `https://api.woovi.com/api/v1/charge/${id}`,
+      {
+        headers: {
+          Authorization: API_ID
+        }
+      }
+    );
+
+    const charge = response.data;
+
+    res.json({
+      success: true,
+      isPaid: charge.status === "COMPLETED"
+    });
+
+  } catch (e) {
+
+    console.log(e.response?.data || e.message);
+
+    res.json({
+      success: false
+    });
+  }
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
