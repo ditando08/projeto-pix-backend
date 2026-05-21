0b45f1f3530dadc1b0b383d5f910b44aa7b9e9a6
Flavio <flaviocsantos99@gmail.com>
2026-05-21 12:36:09 -0300

    alterado comentario

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.post("/criar-pix", async (req, res) => {
 
         value: Number(valor) * 100,
 
-        comment: "cantb",
+        comment: "Cantinho das Borboletas",
 
         customer: {
   name: gerarNome(),
